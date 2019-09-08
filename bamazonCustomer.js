394baa1b8f18c9b46ec36579cd09435aed90ab0a
Isaac Molina <isaacmolina.m16@gmail.com>
2019-09-08 18:18:12 -0500

    added variables into js and a function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,35 @@
+var mysql = require('mysql');
+var inquirer = ('inquirer');
+var table = require('cli-table2');
 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: '',
+    password: '',
+    database: 'bamazon_db',
+    port: 3306,
+})
+connection.connect();
+
+var display = function() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      console.log('-----------------------------');
+      console.log('       Welcome To Bamazon    ');
+      console.log('-----------------------------');
+      console.log('');
+      console.log('Find Your Products Below');
+      console.log('');
+      var table = new Table({
+        head: ['Product Id', 'Product Description', 'Cost'],
+        colWidths: [12, 50, 8],
+        colAligns: ['center', 'left', 'right'],
+        style: {
+          head: ['aqua'],
+          compact: true
+          
+        }
+
+      });
+    });
+}
